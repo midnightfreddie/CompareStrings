ac4460ca1e70b0850563dcfb7f0339e911bedd2f
Jim Nelson <jim@midnightfreddie.com>
2016-12-10 16:09:34 -0600

    Figuring out bigram hash

--- CompareStrings/CompareStrings.psm1
+++ CompareStrings/CompareStrings.psm1
@@ -2,8 +2,14 @@ function Get-Bigrams {
     param (
         [Parameter(Mandatory=$true)][string]$String
     )
-    
-    for ($i = 0; $i -lt $String.Length - 2; $i++) { 
-        "Hi"
+    $Bigrams = @{}
+    for ($i = 0; $i -lt $String.Length - 2; $i++) {
+        $Bigram = $String[$i..($i+1)] 
+        if ($Bigrams[$Bigram]) {
+            $Bigrams[$Bigram] += 1
+        } else {
+            $Bigrams[$Bigram] = 1
+        }
     }
+    Write-Output $Bigrams
 }
